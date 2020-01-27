fb0ece2ea92f47d69d9c4d90477f050718d1e7c0
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-27 19:11:36 +0530

    ADDED : Calculated Total Amount of Win/Loss of 20 days

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -11,20 +11,38 @@ MAX_PERCENT=$((STAKE_PER_DAY+$((STAKE_PER_DAY * 50 /100)) ))
 #!Initializing Variables
 cash=$STAKE_PER_DAY
 
-#!Function to Check Win Or Loss
+#!Function for Daily Betting
 function dailyBet() {
 while [ $cash -gt $MIN_PERCENT ] && [ $cash -lt $MAX_PERCENT ]
 do
 	if [ $((RANDOM % 2)) -eq 1 ]
 	then
 		cash=$((cash+BET))
-		echo "Win  $cash"
 	else
 		cash=$((cash-BET))
-		echo "Loose  $cash"
 	fi
 done
-echo "Total " $cash
+gainCash=$((cash-STAKE_PER_DAY))
+echo $gainCash
 }
 
-dailyBet
+#!function for Monthly Bet
+
+function monthlyGambling() {
+#!loop for 20 Days
+for ((i=1;i<=20;i++))
+do
+	totalAmount=$((totalAmount + $(dailyBet)))
+done
+
+#!Checking for Win or loose
+if [ $totalAmount -gt 0 ]
+then
+	echo "Total Amount Won in 20 Days $totalAmount"
+else
+	echo "Total Amount Loose in 20 Days $totalAmount"
+fi
+}
+
+#!Starting game
+monthlyGambling
