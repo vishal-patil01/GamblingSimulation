1a51ca07278622309245997029de442e6bc18752
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-27 16:16:59 +0530

    ADDED : Check Gambler wins or loss

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -2,6 +2,23 @@
 
 echo "Welcome To Gambling Simulation"
 
-#!Initializing Variables
+#!Initializing Constants
 STAKE_PER_DAY=100
 BET=1
+
+#!Initializing Variables
+cash=$STAKE_PER_DAY
+
+#!Function to Check Win Or Loss
+function dailyBet() {
+if [ $((RANDOM % 2)) -eq 1 ]
+then
+	echo "Win"
+	cash=$((cash+BET))
+else
+	echo "Loose"
+	cash=$((cash-BET))
+fi
+}
+
+dailyBet
