418bd00bd8d834ebb4af9be52e90e8b95eca3f12
Vishal Rajput <vishalpatil01@hotmail.com>
2020-01-27 17:05:17 +0530

    ADDED : Condition to check Gambler win or loose upto DailyLimit

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,24 +1,30 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "Welcome To Gambling Simulation"
 
 #!Initializing Constants
 STAKE_PER_DAY=100
 BET=1
+MIN_PERCENT=$((STAKE_PER_DAY-$((STAKE_PER_DAY * 50 /100)) ))
+MAX_PERCENT=$((STAKE_PER_DAY+$((STAKE_PER_DAY * 50 /100)) ))
 
 #!Initializing Variables
 cash=$STAKE_PER_DAY
 
 #!Function to Check Win Or Loss
 function dailyBet() {
-if [ $((RANDOM % 2)) -eq 1 ]
-then
-	echo "Win"
-	cash=$((cash+BET))
-else
-	echo "Loose"
-	cash=$((cash-BET))
-fi
+while [ $cash -gt $MIN_PERCENT ] && [ $cash -lt $MAX_PERCENT ]
+do
+	if [ $((RANDOM % 2)) -eq 1 ]
+	then
+		cash=$((cash+BET))
+		echo "Win  $cash"
+	else
+		cash=$((cash-BET))
+		echo "Loose  $cash"
+	fi
+done
+echo "Total " $cash
 }
 
 dailyBet
